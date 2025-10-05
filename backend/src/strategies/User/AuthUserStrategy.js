992fe210e203929f87f22c791eec5ebd530e3fb3
Jefferson Perluxo Clemente <jperluxo@gmail.com>
2025-10-05 11:49:13 -0300

    [BACKEND] Correções na implementação do endpoint de login de Usuário

--- backend/src/strategies/User/AuthUserStrategy.js
+++ backend/src/strategies/User/AuthUserStrategy.js
@@ -1,14 +1,27 @@
 const sequelize = require("../../config/dbConfig");
 const UserDAO = require('../../daos/UserDAO');
 
-class DeleteUserStrategy {
-    static async execute(id) {
+class AuthUserStrategy {
+    static async execute(user) {
         const transaction = await sequelize.transaction();
         try {
-            await UserDAO.delete(id, transaction);
+            const data = await UserDAO.findOne({ email: user.email }, transaction);
+            if (user.password !== data.password) {
+                throw new Error('Senha incorreta.');
+            }
+
+            const formattedUser = {
+                id: data.id,
+                name: data.name,
+                email: data.email,
+                password: data.password,
+                cellphone: data.cellphone,
+                type: data.type,
+                createdAt: data.createdAt,
+            };
 
             await transaction.commit();
-            return { status: 200, message: `Usuário ${id} deletado com sucesso!` };
+            return { status: 200, formattedUser };
         } catch (error) {
             await transaction.rollback();
             throw error;
@@ -16,4 +29,4 @@ class DeleteUserStrategy {
     }
 }
 
-module.exports = DeleteUserStrategy;
+module.exports = AuthUserStrategy;
