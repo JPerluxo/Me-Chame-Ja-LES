3c841511578015a07010a73f2be335dd4d4c82de
FelipeOPratinha <felipeaires98@hotmail.com>
2025-10-05 07:16:40 -0300

    [FRONTEND] Adição do context de usuário (Solicitante/Motorista)

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,5 +1,10 @@
-import { ExpoRoot } from 'expo-router';
+import { ExpoRoot } from "expo-router";
+import { AuthProvider } from "./src/context/AuthContext";
 
 export default function App() {
-  return <ExpoRoot context={require.context('./src/app')} />;
+  return (
+    <AuthProvider>
+      <ExpoRoot context={require.context("./src/app")} />
+    </AuthProvider>
+  );
 }
